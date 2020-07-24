88f74bdf677be589aaedccfabdfeeffe9c522a93
eddogustian <eddogustian78@gmail.com>
2020-07-24 21:29:46 +0700

    controller

---
+++ app/Http/Controllers/Admin/PostController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Post;
+use App\Category;
+
+
+class PostController extends Controller
+{
+    //
+}
