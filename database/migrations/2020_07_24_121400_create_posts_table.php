dc530013b3edaadb15c78a3a823704a84caa1a5d
eddogustian <eddogustian78@gmail.com>
2020-07-24 19:31:48 +0700

    schema database

---
+++ database/migrations/2020_07_24_121400_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('id_kategori');
+            $table->string('judul', 250);
+            $table->string('gambar', 250);
+            $table->longText('isi');
+            $table->boolean('status')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
