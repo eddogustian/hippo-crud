d5290ff9472750b5d20d2d210a1df3440601b268
eddogustian <eddogustian78@gmail.com>
2020-07-26 20:29:59 +0700

    user seed

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+use App\User;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'admininitrator',
+            'email' => 'admin@admin.com',
+            'password' => Hash::make('admin123'),
+        ]);
+    }
+}
